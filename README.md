8ee445495e69fb9e93911fdc7b91fcf5d808fbda
hartnetl <laura.hartnett@outlook.com>
2021-07-07 14:37:16 +0000

    Feat: Create get_sales_data fctn to get data string from user

--- README.md
+++ README.md
@@ -9,5 +9,14 @@ It is a market stall which sells sandwiches. They make some beforehand each day 
 keep track of how many are sold (sales), make extra for customers if they run out (negative surplus values) and 
 throw away leftovers (positive surplus values).
 
+## What app should do
+- Collect sales data from user
+- Add sales data into sales worksheet
+- Calculate surplus numbers
+- Add surplus data to worksheet
+- Calculate the average sales for the last 5 markets, to decide how many to make for the next one
+- Print stock recommendations to terminal
+- Check that the sales data input from the user is valid
+
 ## Key features:
 - Link google sheet and google drive APIs to allow Python to access and edit the data
--- run.py
+++ run.py
@@ -12,7 +12,20 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')  # parameter is the name of our spreadsheet doc
 
-sales = SHEET.worksheet('sales')  # Access 'sales' worksheet data in our spreadsheet
+# Below checks API is working. Not needed once checked, delete in normal file
+# sales = SHEET.worksheet('sales')  # Access 'sales' worksheet data in our spreadsheet
+# data = sales.get_all_values()  # get_all_values is a gspread method
+# print(data)
 
-data = sales.get_all_values()  # get_all_values is a gspread method
-print(data)
+def get_sales_data():
+    """
+    Get sales figures input from user
+    """
+    print("Please enter sales data from the last market.")
+    print("Data should be six numbers, separated by commas.")
+    print("Example: 10,20,30,40,50,60\n")
+
+    data_str = input("Enter your data here: ")
+    print(f"The data provided here is {data_str}")
+
+get_sales_data()
